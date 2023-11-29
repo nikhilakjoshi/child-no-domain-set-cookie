9675a2385ccaa0288918ef68eec613590f362635
Nikhil Joshi <nikhilakjoshi@gmail.com>
2023-11-29 12:13:24 -0600

    Set-Cookie

---
+++ src/pages/api/nosetcookie/index.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+type ResponseData = {
+  samlToken?: string;
+  message?: string;
+};
+
+// const rand = function () {
+//   return Math.random().toString(36).substr(2); // remove `0.`
+// };
+
+// const token = function () {
+//   return rand() + rand(); // to make it longer
+// };
+
+const cors = Cors({
+  methods: ["GET", "HEAD", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
+  origin: ["https://onlineaccess.bluebuckdesigns.xyz", "http://localhost:3000"],
+  credentials: true,
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: typeof cors,
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
+) {
+  if (req.method !== "GET")
+    return res.status(405).json({ message: "Method not allowed" });
+  runMiddleware(req, res, cors);
+  //add 2 seconds delay
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  res
+    .status(200)
+    .json({ message: `success - ${new Date().getTime().toString(36)}` });
+}
